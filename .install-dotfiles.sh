d1c45fcf432b36abba2caa1c66672fab261865cb
Bobby R. Ward <bobbyrward@gmail.com>
2015-05-06 10:35:17 -0400

    Playing with the idea of an installation script

---
+++ .install-dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! $(which git) ]; then
+	if [ ! $(which apt-get) ]; then
+		echo "Install git first"
+		exit 1
+	fi
+
+	sudo apt-get install -y git
+fi
+
+DOTFILES_DIR=~/.dotfiles
+
+if [ -d "$DOTFILES_DIR" ]; then
+	rm -rf $DOTFILES_DIR
+fi
+
+git clone https://github.com/bobbyrward/dotfiles.git $DOTFILES_DIR
+
+cd $DOTFILES_DIR
+
+find $DOTFILES_DIR -type f -not -path "$DOTFILES_DIR/.git/*" -exec cp {} ~ \;
+
+source ~/.bashrc
