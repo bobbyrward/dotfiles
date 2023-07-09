f82417b16cba8659d0fce3255764c60cd93bc551
Bobby R. Ward <bobbyrward@gmail.com>
2023-07-09 02:23:41 -0500

    Fix some issues with .initial-setup

--- .initial-setup.sh
+++ .initial-setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-install_rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get install build-essential python3-dev python3-pip neovim
+source $HOME/.cargo/env
+
+sudo add-apt-repository --yes ppa:neovim-ppa/stable
+sudo apt-get install --yes build-essential python3-dev python3-pip neovim
 cargo install exa bat fd-find ripgrep kx fnm
+
+eval "$(fnm env --use-on-cd)"
 
 fnm install 18
 fnm default 18
